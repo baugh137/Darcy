6063f46eee3b5f3e68ffeb2e384eaa144fb12ca1
dbaugh <denisebaugh@u.boisestate.edu>
2018-11-28 22:44:25 -0700

    is this working

--- persuasion.php
+++ persuasion.php
@@ -5,7 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   	    <link href="base.css" type="text/css" rel="stylesheet" />
 		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		  
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
   	<link href="https://fonts.googleapis.com/css?family=Acme|Caveat|Cedarville+Cursive|Dancing+Script|Great+Vibes" rel="stylesheet" type="text/css">
 
   
