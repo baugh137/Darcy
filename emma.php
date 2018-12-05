e9abc3e9a69e3dcd187b5886615c97cf6653a2a0
dbaugh <denisebaugh@u.boisestate.edu>
2018-12-05 12:00:33 -0700

    update

--- emma.php
+++ emma.php
@@ -3,11 +3,11 @@
 	<title>Darcy Approved, Collins Detected: Emma Dogtectors</title>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  	   
+  	    <link href="base.css" type="text/css" rel="stylesheet" />
 		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<link href="https://fonts.googleapis.com/css?family=Acme|Caveat|Cedarville+Cursive|Dancing+Script|Great+Vibes" rel="stylesheet" type="text/css">
-   <link href="base.css" type="text/css" rel="stylesheet" />
+  
 	</head>
 	<body>
 	<?php include "header.php"; ?>
@@ -23,7 +23,7 @@
 <div class="container">
   <h2>Ready to take him home?</h2>
   <a href="addEmma.php" class="btn btn-info" role="button">Add to Basket</a>
-  <!--<input type="submit" class="btn btn-info" value="Submit Button"> -->
+  
 </div>	
 </div>
 	
